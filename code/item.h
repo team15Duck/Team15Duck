3e9a9b81b2d0491075239282e199929c95ca99ae
jjinsword <jjinsword@gmail.com>
2019-01-16 12:31:03 +0900

    - filed item
    - filed item 작업중

--- code/item.h
+++ code/item.h
@@ -1,11 +1,44 @@
 #pragma once
+
+enum ITEM_TYPE
+{
+	ITEM_TYPE_FRUIT,	// 과일
+	ITEM_TYPE_MEAT,		// 고기
+	ITEM_TYPE_SHEILD,	// 실드
+	ITEM_TYPE_KEY,		// 열쇠
+	ITEM_TYPE_LOCK,		// 자물쇠
+
+	ITEM_TYPE_NONE,
+	ITEM_TYPE_COUNT = ITEM_TYPE_NONE,
+};
+
 class item
 {
 private:
 
+	float _x;				// 중점좌표
+	float _y;
+
+	ITEM_TYPE	_type;		// 아이템 타입
+	int			_value;		// 능력치(ex.  FRUIT _value = 1 : hp를 1 채워줌
+							//			   MEAT  _value = 2 : hp를 2 채워줌
+							//			    KEY _value == LOCK _value >> 문이 열린다		
+
+	image* _img;			// 이미지
+	POINT _size;			// 크기
+	RECT _rc;				// 충돌 렉트
 
 public:
 	item();
 	~item();
+
+	HRESULT init(const char* imgName, POINTF position, int itemValue);
+	void release();
+	void update();
+	void render();
+
+	ITEM_TYPE getItemType() { return _type;	 }
+	int getItemValue()		{ return _value; }
+	RECT getItemRect()		{ return _rc;	 }
 };
 
