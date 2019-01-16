78287f9f2fa244d40780f99628412dce4a0d7d62
fairychoi <ccw5763@gmail.com>
2019-01-16 10:41:06 +0900

    유틸에 기본 매크로 추가 및 매크로 펑션 헤더파일 삭제

--- code/utils.cpp
+++ code/utils.cpp
@@ -4,7 +4,74 @@
 
 namespace TEAM_15DUCK_UTILL
 {
+	POINT PointMake(int x, int y)
+	{
+		POINT pt = { x, y };
 
+		return pt;
+	}
+	POINTF PointFMake(float x, float y)
+	{
+		POINTF pt = { x, y };
+
+		return pt;
+	}
+	void LineMake(HDC hdc, int startX, int startY, int endX, int endY)
+	{
+		MoveToEx(hdc, startX, startY, NULL);
+		LineTo(hdc, endX, endY);
+	}
+	void LineMake(HDC hdc, POINTF start, POINTF end)
+	{
+		MoveToEx(hdc, start.x, start.y, NULL);
+		LineTo(hdc, end.x, end.y);
+	}
+	RECT RectMake(int x, int y, int width, int height)
+	{
+		RECT rc = { x, y, x + width, y + height };
+		return rc;
+	}
+	RECT RectMakeCenter(int x, int y, int width, int height)
+	{
+		RECT rc = { x - (width / 2), y - (height / 2), x + (width / 2), y + (height / 2) };
+		return rc;
+	}
+	void RectangleMake(HDC hdc, int x, int y, int width, int height)
+	{
+		Rectangle(hdc, x, y, x + width, y + height);
+	}
+	void RectangleMakeCenter(HDC hdc, int x, int y, int width, int height)
+	{
+		Rectangle(hdc, x - (width / 2), y - (height / 2), x + (width / 2), y + (height / 2));
+	}
+	void Rectangle(HDC hdc, RECT & rc)
+	{
+		Rectangle(hdc, rc.left, rc.top, rc.right, rc.bottom);
+	}
+	ELLIPSE EllipseMake(float x, float y, float r)
+	{
+		ELLIPSE rt;
+		rt.p.x = x;
+		rt.p.y = y;
+		rt.r = r;
+		return rt;
+	}
+	void EllipseCenterRender(HDC hdc, int x, int y, int width, int height)
+	{
+		Ellipse(hdc, x - (width / 2), y - (height / 2), x + (width / 2), y + (height / 2));
+	}
+	void EllipseRender(HDC hdc, ELLIPSE & e)
+	{
+		Ellipse(hdc, e.p.x - e.r, e.p.y - e.r, e.p.x + e.r, e.p.y + e.r);
+	}
+	float RadToDeg(float angle)
+	{
+		return angle * (180.0f / PI);
+	}
+	float DegToRad(float angle)
+	{
+		return angle * (PI / 180.0f);
+	}
 	//거리 구하는 함수
 	float getDistance(float x1, float y1, float x2, float y2)
 	{
@@ -14,28 +81,236 @@ namespace TEAM_15DUCK_UTILL
 		return sqrtf(x * x + y * y);
 	}
 
-	float getAngle(float x1, float y1, float x2, float y2)
+	float getDistance(POINTF & p1, POINTF & p2)
 	{
+		return sqrtf((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+	}
 
-		float x = x2 - x1;
-		float y = y2 - y1;
+	bool EllipseCollision(ELLIPSE & e1, ELLIPSE & e2)
+	{
+		return (e1.r + e2.r >= getDistance(e1.p, e2.p));
+	}
 
-		float distance = sqrtf(x * x + y * y);
+	float getCenter(float a, float b)
+	{
+		return a + (b - a) / 2;
+	}
 
-		//아탄투
-		//float angle = -atan2f(y, x);
+	POINT getRectCenter(RECT & rc)
+	{
+		POINT p;
+		p.x = getCenter(rc.left, rc.right);
+		p.y = getCenter(rc.top, rc.bottom);
+		return p;
+	}
 
-		float angle = acosf(x / distance);
+	float getRectXCenter(RECT & rc)
+	{
+		return getCenter(rc.left, rc.right);
+	}
 
-		//예외처리 필쑤
-		if (y2 > y1)
+	float getRectYCenter(RECT & rc)
+	{
+		return getCenter(rc.top, rc.bottom);
+	}
+
+	float getRectWidth(RECT & rc)
+	{
+		return rc.right - rc.left;
+	}
+
+	float getRectHeight(RECT & rc)
+	{
+		return rc.bottom - rc.top;
+	}
+
+	bool PtInEllipse(ELLIPSE & e, POINTF p)
+	{
+		return (e.r >= getDistance(e.p, p));
+	}
+
+	bool PtInEllipse(ELLIPSE & e, float x, float y)
+	{
+		return (e.r >= getDistance(e.p.x, e.p.y, x, y));
+	}
+
+	float dCosf(float angle)
+	{
+		return cosf(DegToRad(angle));
+	}
+
+	float dSinf(float angle)
+	{
+		return sinf(DegToRad(angle));
+	}
+
+	float getAngle(POINTF p, POINTF target)
+	{
+		return -atan2f(target.y - p.y, target.x - p.x);
+	}
+
+	float getAngle(float x, float y, float x2, float y2)
+	{
+		return -atan2f(y2 - y, x2 - x);
+	}
+
+	float getAngle2(POINTF p, POINTF target)
+	{
+		float angle;
+		float deltaX = target.x - p.x;
+		float deltaY = target.y - p.y;
+		float distance = sqrtf(deltaX* deltaX + deltaY * deltaY);
+		angle = acosf(deltaX / distance);
+		if (deltaY < 0)
 		{
 			angle = 2 * PI - angle;
-			if (angle >= PI * 2) angle -= PI * 2;
+			if (angle > 2 * PI) angle -= 2 * PI;
 		}
-
-
 		return angle;
+	}
+
+	float getAngle2(float x, float y, float x2, float y2)
+	{
+		float angle;
+		float deltaX = x2 - x;
+		float deltaY = y2 - y;
+		float distance = sqrtf(deltaX* deltaX + deltaY * deltaY);
+		angle = acosf(deltaX / distance);
+		if (deltaY < 0)
+		{
+			angle = 2 * PI - angle;
+			if (angle > 2 * PI) angle -= 2 * PI;
+		}
+		return angle;
+	}
+
+	float dGetAngle(POINTF target, POINTF p)
+	{
+		return RadToDeg(-atan2f(target.y - p.y, target.x - p.x));
+	}
+
+	float dGetAngle(float x2, float y2, float x, float y)
+	{
+		return RadToDeg(-atan2f(y2 - y, x2 - x));
+	}
+
+	float dGetAngle2(POINTF target, POINTF p)
+	{
+		float angle;
+		float deltaX = target.x - p.x;
+		float deltaY = target.y - p.y;
+		float distance = sqrtf(deltaX* deltaX + deltaY * deltaY);
+		angle = acosf(deltaX / distance);
+		if (deltaY < 0)
+		{
+			angle = 2 * PI - angle;
+		}
+		return RadToDeg(angle);
+	}
+
+	float dGetAngle2(float x2, float y2, float x, float y)
+	{
+		float angle;
+		float deltaX = x2 - x;
+		float deltaY = y2 - y;
+		float distance = sqrtf(deltaX* deltaX + deltaY * deltaY);
+		angle = acosf(deltaX / distance);
+		if (deltaY < 0)
+		{
+			angle = 2 * PI - angle;
+		}
+		return RadToDeg(angle);
+	}
+
+	bool RectEllipseCollition(RECT & rc, ELLIPSE & e)
+	{
+		if ((rc.left <= e.p.x && e.p.x <= rc.right) || (rc.top <= e.p.y && e.p.y <= rc.bottom))
+		{
+			POINT p;
+			p.x = e.p.x;
+			p.y = e.p.y;
+			RECT rc2 = { rc.left - e.r, rc.top - e.r, rc.right + e.r, rc.bottom + e.r };
+			if (PtInRect(&rc2, p))return true;
+		}
+		else
+		{
+			POINTF p;
+			p.x = rc.left;
+			p.y = rc.top;
+			if (PtInEllipse(e, p)) return true;
+			p.x = rc.left;
+			p.y = rc.bottom;
+			if (PtInEllipse(e, p)) return true;
+			p.x = rc.right;
+			p.y = rc.top;
+			if (PtInEllipse(e, p)) return true;
+			p.x = rc.right;
+			p.y = rc.bottom;
+			if (PtInEllipse(e, p)) return true;
+		}
+		return false;
+	}
+
+	void RectangleBrush(HDC hdc, RECT & rc, COLORREF color)
+	{
+		HBRUSH brush = CreateSolidBrush(color);
+		HBRUSH oBrush = (HBRUSH)SelectObject(hdc, brush);
+		Rectangle(hdc, rc);
+		SelectObject(hdc, oBrush);
+		DeleteObject(brush);
+	}
+
+	void RectangleBrushPen(HDC hdc, RECT & rc, COLORREF color)
+	{
+		HBRUSH brush = CreateSolidBrush(color);
+		HBRUSH oBrush = (HBRUSH)SelectObject(hdc, brush);
+		HPEN pen = CreatePen(PS_SOLID, 1, color);
+		HPEN oPen = (HPEN)SelectObject(hdc, pen);
+		Rectangle(hdc, rc);
+		SelectObject(hdc, oBrush);
+		DeleteObject(brush);
+		SelectObject(hdc, oPen);
+		DeleteObject(pen);
+	}
+
+	void RectanglePen(HDC hdc, RECT & rc, COLORREF color)
+	{
+		HPEN pen = CreatePen(PS_SOLID, 1, color);
+		HPEN oPen = (HPEN)SelectObject(hdc, pen);
+		Rectangle(hdc, rc);
+		SelectObject(hdc, oPen);
+		DeleteObject(pen);
+	}
+
+	void EllipseBrush(HDC hdc, ELLIPSE & e, COLORREF color)
+	{
+		HBRUSH brush = CreateSolidBrush(color);
+		HBRUSH oBrush = (HBRUSH)SelectObject(hdc, brush);
+		EllipseRender(hdc, e);
+		SelectObject(hdc, oBrush);
+		DeleteObject(brush);
+	}
+
+	void EllipseBrushPen(HDC hdc, ELLIPSE & e, COLORREF color)
+	{
+		HBRUSH brush = CreateSolidBrush(color);
+		HBRUSH oBrush = (HBRUSH)SelectObject(hdc, brush);
+		HPEN pen = CreatePen(PS_SOLID, 1, color);
+		HPEN oPen = (HPEN)SelectObject(hdc, pen);
+		EllipseRender(hdc, e);
+		SelectObject(hdc, oBrush);
+		DeleteObject(brush);
+		SelectObject(hdc, oPen);
+		DeleteObject(pen);
+	}
+
+	void EllipsePen(HDC hdc, ELLIPSE & e, COLORREF color)
+	{
+		HPEN pen = CreatePen(PS_SOLID, 1, color);
+		HPEN oPen = (HPEN)SelectObject(hdc, pen);
+		EllipseRender(hdc, e);
+		SelectObject(hdc, oPen);
+		DeleteObject(pen);
 	}
 
 }
--- code/utils.h
+++ code/utils.h
@@ -10,17 +10,128 @@
 #define PI64	float(PI / 64.0f)
 #define PI128	float(PI / 128.0f)
 
+//float 형 포인트 변수
+typedef struct tagPointFloatInfo
+{
+	float x;
+	float y;
+}POINTF, *PPOINTF;
+//원
+typedef struct tagELLIPSE
+{
+	POINTF p;
+	float r;
+} ELLIPSE, *LPELLIPSE;
+
 namespace TEAM_15DUCK_UTILL
 {
-	typedef struct tagPointFloatInfo
-	{
-		float x;
-		float y;
-	}POINTF, *PPOINTF;
+	inline POINT PointMake(int x, int y);
+
+	inline POINTF PointFMake(float x, float y);
+
+	inline void LineMake(HDC hdc, int startX, int startY, int endX, int endY);
+
+	inline void LineMake(HDC hdc, POINTF start, POINTF end);
 
 
+	//================= 렉트 관련 함수 ========================
+	//    렉트를 만든다(레프트, 탑, 가로크기, 세로크기)
+	inline RECT RectMake(int x, int y, int width, int height);
+	//    렉트를 만든다(중점X좌표, 중점Y좌표, 가로크기, 세로크기)
+	inline RECT RectMakeCenter(int x, int y, int width, int height);
+	//================= 사각형 관련 함수 ======================
+	//          사각형 생성함수(뿌려줄DC, left, top, 가로크기, 세로크기
+	inline void RectangleMake(HDC hdc, int x, int y, int width, int height);
 
+	inline void RectangleMakeCenter(HDC hdc, int x, int y, int width, int height);
+
+	inline void Rectangle(HDC hdc, RECT& rc);
+
+	//====================== 동 그 라 미 관련 ===============================
+	//        원 생성함수 (hdc, left, top, 가로크기, 세로크기)
+	inline ELLIPSE EllipseMake(float x, float y, float r);
+	//        원 생성함수 (hdc, 센터좌표 X, 센터좌표 Y, 가로크기, 세로크기) //타원가능
+	inline void EllipseCenterRender(HDC hdc, int x, int y, int width, int height);
+	//        원 그림
+	inline void EllipseRender(HDC hdc, ELLIPSE& e);
+	//라디안 각도를 디그리로
+	inline float RadToDeg(float angle);
+
+	//디그리 각도를 라디안으로
+	inline float DegToRad(float angle);
+
+	//거리구하기
 	float getDistance(float x1, float y1, float x2, float y2);
+	inline float getDistance(POINTF& p1, POINTF& p2);
+	
+	//원충돌했는지
+	inline bool EllipseCollision(ELLIPSE& e1, ELLIPSE& e2);
 
-	float getAngle(float x1, float y1, float x2, float y2);
+	//중점구하기
+	inline float getCenter(float a, float b);
+
+	//렉트 중점 반환함수
+	inline POINT getRectCenter(RECT& rc);
+
+	//렉트 x중점 반환 함수
+	inline float getRectXCenter(RECT& rc);
+	
+	//렉트 y중점 반환 함수
+	inline float getRectYCenter(RECT& rc);
+
+	//렉트 width 반환 함수
+	inline float getRectWidth(RECT& rc);
+
+	//렉트 height 반환 함수
+	inline float getRectHeight(RECT& rc);
+
+	//원 점 충돌
+	inline bool PtInEllipse(ELLIPSE& e, POINTF p);
+
+	//원 점 충돌
+	inline bool PtInEllipse(ELLIPSE& e, float x, float y);
+
+	//디그리 코사인
+	inline float dCosf(float angle);
+
+	//디그리 사인
+	inline float dSinf(float angle);
+
+	//원두개 박아서 각도받아오기 아크탄젠트 반환형 라디안
+	inline float getAngle(POINTF p, POINTF target);
+
+	inline float getAngle(float x, float y, float x2, float y2);
+
+	//원 두개 박아서 각도받아오기 아크코사인 반환형 라디안
+	inline float getAngle2(POINTF p, POINTF target);
+
+	inline float getAngle2(float x, float y, float x2, float y2);
+
+	//원두개 박아서 각도받아오기 아크탄젠트 반환형 디그리
+	inline float dGetAngle(POINTF target, POINTF p);
+
+	inline float dGetAngle(float x2, float y2, float x, float y);
+
+	//원 두개 박아서 각도받아오기 아크코사인 반환형 디그리
+	inline float dGetAngle2(POINTF target, POINTF p);
+
+	inline float dGetAngle2(float x2, float y2, float x, float y);
+
+	//원 사각형 충돌
+	inline bool RectEllipseCollition(RECT& rc, ELLIPSE& e);
+
+	//렉트 색칠
+	inline void RectangleBrush(HDC hdc, RECT& rc, COLORREF color);
+
+	inline void RectangleBrushPen(HDC hdc, RECT& rc, COLORREF color);
+
+	inline void RectanglePen(HDC hdc, RECT& rc, COLORREF color);
+
+	//원색칠
+	inline void EllipseBrush(HDC hdc, ELLIPSE& e, COLORREF color);
+
+	inline void EllipseBrushPen(HDC hdc, ELLIPSE& e, COLORREF color);
+
+	inline void EllipsePen(HDC hdc, ELLIPSE& e, COLORREF color);
+
 }
